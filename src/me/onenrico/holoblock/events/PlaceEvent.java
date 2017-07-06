8c2ad74b19615df3a281202771032c8fe36d0c5e
Enrico <Enrico@172.30.0.1>
2017-07-06 20:03:35 +0700

    Add EventPriority on place

--- src/me/onenrico/holoblock/events/PlaceEvent.java
+++ src/me/onenrico/holoblock/events/PlaceEvent.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
@@ -28,7 +29,7 @@ import me.onenrico.holoblock.utils.PlayerUT;
 
 
 public class PlaceEvent implements Listener {
-	@EventHandler
+	@EventHandler (priority = EventPriority.HIGHEST)
 	public void playerPlace(final BlockPlaceEvent event) {
 		Player player = event.getPlayer();
 		ItemStack hand = PlayerUT.getHand(player);
